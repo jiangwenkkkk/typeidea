d0f84a62941d46060f6286ef8af2bd62b1874e29
jianghn <jiangwenaini@gmail.com>
2020-05-19 15:14:42 +0800

    增加项目说明

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
 #    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeieda.settings')
     profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings.%s' % profile)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeieda.settings.%s' % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
